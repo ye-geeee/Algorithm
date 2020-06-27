1efd641923e9283e4334083dcde227995fe5f7e4
ye-geeee <yeaji9683@gmail.com>
2020-06-27 17:22:38 +0900

    2020.06.07 Coin Change 2

---
+++ leetcode/June2020/src/main/java/Day7_Coin_Change_2.java
@@ -0,0 +1,29 @@
+public class Day7_Coin_Change_2 {
+    class Solution {
+        public int result[][];
+
+        public int change(int amount, int[] coins) {
+            result = new int[coins.length+1][amount+1];
+
+            for(int i = 0; i <= coins.length; i++){
+                result[i][0] = 1;
+            }
+
+            for(int i = 1; i <= coins.length; i++){
+                for(int j = 1; j <= amount; j++){
+                    result[i][j] += result[i-1][j];
+                    if(j-coins[i-1] >= 0) result[i][j] += result[i][j-coins[i-1]];
+                }
+            }
+
+            return result[coins.length][amount];
+        }
+    }
+
+    public static void main(String[] args) {
+        int tc[] = {1, 2, 5};
+
+        Day7_Coin_Change_2.Solution sol = new Day7_Coin_Change_2().new Solution();
+        System.out.println(sol.change(5, tc));
+    }
+}
