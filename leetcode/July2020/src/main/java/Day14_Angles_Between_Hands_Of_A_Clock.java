97db1eafd26cc4793807292a40294a5905ad7403
ye-geeee <yeaji9683@gmail.com>
2020-07-16 06:59:45 +0900

    2020.07.14 Angles Between Hands of a Clock

---
+++ leetcode/July2020/src/main/java/Day14_Angles_Between_Hands_Of_A_Clock.java
@@ -0,0 +1,13 @@
+public class Day14_Angles_Between_Hands_Of_A_Clock {
+
+    class Solution {
+        public double angleClock(int hour, int minutes) {
+            return Math.min(Math.abs((minutes * 6) - ((hour % 12) * 30 + 0.5 * minutes)), 360 - Math.abs((minutes * 6) - ((hour % 12) * 30 + 0.5 * minutes)));
+        }
+    }
+
+    public static void main(String[] args) {
+        Day14_Angles_Between_Hands_Of_A_Clock.Solution sol = new Day14_Angles_Between_Hands_Of_A_Clock().new Solution();
+
+    }
+}
