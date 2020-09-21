a864a2b542dbf0de233a5aa6543f527fda17fe20
ye-geeee <yeaji9683@gmail.com>
2020-09-22 01:02:32 +0900

    Remove programmers directory

---
+++ leetcode/July2020/src/main/java/Day2_Binary_Tree_Level_Order_Traversal2.java
@@ -0,0 +1,7 @@
+public class Day2_Binary_Tree_Level_Order_Traversal2 {
+
+    public static void main(String[] args) {
+        Day01_ArrangingCoins.Solution sol = new Day01_ArrangingCoins().new Solution();
+        //System.out.println(sol.arrangeCoins(1804289383));
+    }
+}
