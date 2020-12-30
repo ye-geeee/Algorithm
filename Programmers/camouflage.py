3369530eb2dd4db695034ef04370e8874eef8da5
ye-geeee <yeaji9683@gmail.com>
2020-12-30 20:25:31 +0900

    Add Change

--- Programmers/camouflage.py
+++ Programmers/camouflage.py
@@ -5,5 +5,5 @@ def solution(clothes):
     return reduce(lambda x, y : x * (y+1), Counter(kind for name, kind in clothes).values(), 1) - 1
 
 if __name__ == '__main__':
-     clothes = [["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]
-     print(solution(clothes))
+    clothes = [["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]
+    print(solution(clothes))
