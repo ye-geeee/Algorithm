83eb9f882eca7d70fb985f063ae1b97e45b2953f
ye-geeee <yeaji9683@gmail.com>
2021-01-03 23:18:14 +0900

    [Leetcode] 2021.01.03 Beautiful Arrangement (Jan Challenge Day 03)

---
+++ leetcode/src/main/java/January2021/Day03_Beautiful_Arrangement.java
@@ -0,0 +1,28 @@
+package January2021;
+
+public class Day03_Beautiful_Arrangement {
+    public static void main(String[] args) {
+        System.out.println(new Day03_Beautiful_Arrangement.Solution().countArrangement(2));
+    }
+
+    static class Solution {
+        public int countArrangement(int n) {
+            boolean[] visited = new boolean[n + 1];
+            return countArrangementRecursive(n, 1, visited);
+        }
+
+        public int countArrangementRecursive(int n, int index, boolean[] visited) {
+            if (index > n) return 1;
+            int res = 0;
+
+            for (int i = 1; i <= n; i++) {
+                if (!visited[i] && (index % i == 0 || i % index == 0)) {
+                    visited[i] = true;
+                    res += countArrangementRecursive(n, index + 1, visited);
+                    visited[i] = false;
+                }
+            }
+            return res;
+        }
+    }
+}
